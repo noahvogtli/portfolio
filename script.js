40a5db56ac7a219b9d315ca935eb25822c972866
Noah Vogtli <noahvogtli88@gmail.com>
2024-02-27 23:10:45 -0500

    styled the navbar

--- script.js
+++ script.js
@@ -6,19 +6,6 @@ window.onscroll = function() {
 // Get the header
 var header = document.getElementById("myHeader");
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.scrollY > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-}
-
-
 function getFileType()
 {
     var fileType = document.getElementById("filetype").value;
