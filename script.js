14751064b16d05ac530cac0911584b2cb0765a7e
Noah Vogtli <noahvogtli88@gmail.com>
2024-02-28 14:19:20 -0500

    fixed the download js function to download the correct file type

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {
-    myFunction();
-};
-
-// Get the header
-var header = document.getElementById("myHeader");
-
 function getFileType()
 {
     var fileType = document.getElementById("filetype").value;
-    var downloadLink = document.querySelector(".download-button");
+    var downloadLink = document.getElementById('download-button');
 
     downloadLink.href = "data/Noah_Vogtli_Resume." + fileType;
 }
